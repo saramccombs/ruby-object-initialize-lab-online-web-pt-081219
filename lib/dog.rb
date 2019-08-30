15e924750b2fe3f01aaf8927da03873faa961d56
Sara McCombs <thesaramccombs@gmail.com>
2019-08-30 20:08:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,10 @@
 class Dog
-  def initialize(name)
+  def initialize(name, breed)
     @name = name
+    @breed = breed
   end
   
-  def name=(name)
-    @name = name
-  end
-  
-  def name
-    @name
-  end
+  attr_accessor :name
+  attr_accessor :breed
 
 end
